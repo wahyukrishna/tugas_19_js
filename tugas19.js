4bfc80bc0b82e0d2a5cd6ea88f378706f489eb98
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-06 21:00:27 +0700

    Tugas 19 JS
    Tugas Niomic

---
+++ tugas19.js
@@ -0,0 +1,21 @@
+
+const tugas19 = () => {
+    //soal no.1
+    console.log('Semangat...'.repeat(5));
+    console.log('--------------------------');
+    
+    //soal.no.2
+    var repeats = '*'.repeat(5);
+    console.log(repeats);
+    console.log(repeats.repeat(5));
+    console.log(repeats.repeat(9));
+    console.log(repeats.repeat(13));
+    console.log(repeats.repeat(19));
+    console.log(repeats.repeat(13));
+    console.log(repeats.repeat(9));
+    console.log(repeats.repeat(5));
+    console.log(repeats);
+
+}
+
+tugas19();
